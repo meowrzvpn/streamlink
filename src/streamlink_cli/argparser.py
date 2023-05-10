faa1fd6e59dd949474deb7e21cc1a6e5524fa5cc
meowrzvpn <88828657+meowrzvpn@users.noreply.github.com>
2023-05-11 00:25:28 +0800

    Update argparser.py

--- src/streamlink_cli/argparser.py
+++ src/streamlink_cli/argparser.py
@@ -1093,8 +1093,21 @@ def build_parser():
         help="""
         Use a CENC decryption key to decrypt the media that ffmpeg receives as
         an input from the DASH streaming that you play with streamlink.
+        If only one decryption key is provided, it will be used for both video and audio.
+        If --decryption_key_2 is also provided, it will be used for the first track.
 
         Example: -decryption_key "<hex key>"
+        """
+    )
+    transport_ffmpeg.add_argument(
+        "-decryption_key_2",
+        metavar="FILENAME",
+        help="""
+        Use a CENC decryption key to decrypt the media that ffmpeg receives as
+        an input from the DASH streaming that you play with streamlink.
+        This key will be used for the second track only.
+
+        Example: -decryption_key_2 "<hex key>"
         """
     )
     transport_ffmpeg.add_argument(
