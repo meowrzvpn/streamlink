056751486cf6b13ced48f23a95e2dc693fba2c82
meowrzvpn <88828657+meowrzvpn@users.noreply.github.com>
2023-05-10 23:40:04 +0800

    Update dash.py

--- src/streamlink/stream/dash.py
+++ src/streamlink/stream/dash.py
@@ -288,8 +288,6 @@ class DASHStream(Stream):
 
         # Search for suitable video and audio representations
         for aset in mpd.periods[period].adaptationSets:
-            if aset.contentProtections:
-                raise PluginError(f"{source} is protected by DRM")
             for rep in aset.representations:
                 if rep.contentProtections:
                     raise PluginError(f"{source} is protected by DRM")
